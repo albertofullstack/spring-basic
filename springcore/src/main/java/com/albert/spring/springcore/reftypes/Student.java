38aa02ce1f0412ec193b88ca34d676be6ad5d3bd
albertofullstack <alberto.fullstack@gmail.com>
2025-03-12 20:34:14 +0100

    Add reference types example.

---
+++ springcore/src/main/java/com/albert/spring/springcore/reftypes/Student.java
@@ -0,0 +1,14 @@
+package com.albert.spring.springcore.reftypes;
+
+public class Student {
+
+  private Scores scores;
+
+  public Scores getScores() {
+    return scores;
+  }
+
+  public void setScores(Scores scores) {
+    this.scores = scores;
+  }
+}
