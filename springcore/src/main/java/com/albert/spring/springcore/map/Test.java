752a8112ca4c0e8cdb8ab9580e6243de1cbec5a4
albertofullstack <alberto.fullstack@gmail.com>
2025-03-12 20:07:31 +0100

    Move .gitignore to project root folder. Add map example.

---
+++ springcore/src/main/java/com/albert/spring/springcore/map/Test.java
@@ -0,0 +1,13 @@
+package com.albert.spring.springcore.map;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+  public static void main(String[] args) {
+    ApplicationContext context = new ClassPathXmlApplicationContext("mapconfig.xml");
+    Customer customer = (Customer) context.getBean("customer");
+    System.out.println(customer);
+  }
+}
