537d46ad2c4fc5582a4f7fd5650511076d01a3a7
Alberto Morante <amoranteb@minsait.com>
2025-03-13 11:12:11 +0100

    Add example of life cycle spring methods.

---
+++ springcore/src/main/java/com/albert/spring/springcore/xmlconfig/Patient.java
@@ -0,0 +1,30 @@
+package com.albert.spring.springcore.xmlconfig;
+
+public class Patient {
+
+  private int id;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    System.out.println("Inside setter method");
+    this.id = id;
+  }
+
+  public void hi() {
+    System.out.println("Inside hi method");
+  }
+
+  public void bye() {
+    System.out.println("Inside bye method");
+  }
+
+  @Override
+  public String toString() {
+    return "Patient{" +
+        "id=" + id +
+        '}';
+  }
+}
---
+++ springcore/src/main/java/com/albert/spring/springcore/xmlconfig/Test.java
@@ -0,0 +1,16 @@
+package com.albert.spring.springcore.xmlconfig;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+  public static void main(String[] args) {
+    AbstractApplicationContext context = new ClassPathXmlApplicationContext("xmlconfig.xml");
+    Patient patient = (Patient) context.getBean("patient");
+    System.out.println(patient.getId());
+
+    // this tells the spring container that it needs to invoke the destroy method
+    context.registerShutdownHook();
+  }
+}
